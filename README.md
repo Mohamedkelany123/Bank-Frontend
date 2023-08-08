cab23fca6421735eef0605809466ee6c771907ec
Mohamedkelany123 <mohamedkelany479@gmail.com>
2023-08-08 12:37:46 +0300

    Login Page Created

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# Bank-Frontend
- Front-end for Bank_Loans_Django_Backend
+# bank-backend
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,73 @@
+<template>
+  <v-app>
+    <div class="background"></div>
+    <v-main class="d-flex justify-center align-center">
+      <v-col cols="10" lg="4" class="mx-auto">
+        <v-card class="pa-4">
+          <div class="text-center">
+            <v-avatar size="100" color="indigo lighten-4">
+              <v-icon size="40" icon="mdi-account"></v-icon>
+            </v-avatar>
+            <h2 class="indigo--text">Login Page</h2>
+          </div>
+          <v-form @submit.prevent="submitHandler">
+            <v-card-text>
+              <v-text-field
+                type="text"
+                label="Username"
+                placeholder="Username"
+                prepend-inner-icon="mdi-account"
+              />
+              <v-text-field
+                :type="passwordShow?'text' : 'password'"
+                label="Password"
+                placeholder="Password"
+                prepend-inner-icon="mdi-key"
+                :append-icon="passwordShow ? 'mdi-eye':'mdi-eye-off'"
+                @click:append="passwordShow = !passwordShow"
+              />
+            </v-card-text>
+              <v-card-actions class="justify-center">
+                <v-btn type="submit" :loading="loading" color="indigo lighten-4" variant="text"  block rounded="lg" size="x-large">
+                    Login
+                </v-btn>
+              </v-card-actions>
+          </v-form>
+        </v-card>
+      </v-col>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    loading: false,
+    passwordShow:false,
+    loginSuccess: false,
+    
+  }),
+  methods:{
+    submitHandler(){
+      console.log(this.Username)
+      this.loginSuccess = true;
+    }
+  }
+}
+</script>
+
+<style>
+  .background{
+    background-image: url(./assets/Order-Banner.jpg) !important;
+    height: 370px;
+    width: 100%;
+    display: block;
+    position: absolute;
+    top: 0;
+    background-size: cover;
+  }
+
+</style>
