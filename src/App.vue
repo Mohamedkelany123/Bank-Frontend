2bbc7cd80e26bc9f3e42ec14228264d64de7fe8f
Mohamedkelany123 <mohamedkelany479@gmail.com>
2023-08-08 12:44:04 +0300

    Upgraded The App.Vue Login Page

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,50 @@
 <template>
   <v-app>
-    <div class="background"></div>
+    <div class="backgruond"></div>
     <v-main class="d-flex justify-center align-center">
       <v-col cols="10" lg="4" class="mx-auto">
         <v-card class="pa-4">
           <div class="text-center">
             <v-avatar size="100" color="indigo lighten-4">
-              <v-icon size="40" icon="mdi-account"></v-icon>
+              <v-icon size="40" >mdi-account</v-icon>
             </v-avatar>
             <h2 class="indigo--text">Login Page</h2>
           </div>
-          <v-form @submit.prevent="submitHandler">
+          <v-form @submit.prevent="submitHandler" ref="form">
             <v-card-text>
               <v-text-field
+                v-model="Text"
+                :rules="usernameRules"
                 type="text"
                 label="Username"
                 placeholder="Username"
                 prepend-inner-icon="mdi-account"
+                required
               />
               <v-text-field
-                :type="passwordShow?'text' : 'password'"
-                label="Password"
-                placeholder="Password"
-                prepend-inner-icon="mdi-key"
-                :append-icon="passwordShow ? 'mdi-eye':'mdi-eye-off'"
-                @click:append="passwordShow = !passwordShow"
+                      v-model="password"
+                      :rules="passwordRules"
+                      :type="passwordShow?'text':'password'"
+                      label="Password"
+                      placeholder="Password"
+                      prepend-inner-icon="mdi-key"
+                      :append-icon="passwordShow ? 'mdi-eye':'mdi-eye-off'"
+                      @click:append="passwordShow = !passwordShow"
+                      required
               />
             </v-card-text>
-              <v-card-actions class="justify-center">
-                <v-btn type="submit" :loading="loading" color="indigo lighten-4" variant="text"  block rounded="lg" size="x-large">
-                    Login
-                </v-btn>
-              </v-card-actions>
+            <v-card-actions class="justify-center">
+              <v-btn :loading="loading" type="submit" color="indigo lighten-4" variant="text"  block rounded="lg" size="x-large">
+                <span>Login</span>
+              </v-btn>
+            </v-card-actions>
           </v-form>
         </v-card>
       </v-col>
     </v-main>
+    <v-snackbar top color="green" v-model="snackbar">
+      Login success
+    </v-snackbar>
   </v-app>
 </template>
 
@@ -44,30 +53,43 @@
 export default {
   name: 'App',
 
+
   data: () => ({
-    loading: false,
+    loading:false,
+    snackbar:false,
     passwordShow:false,
-    loginSuccess: false,
-    
+    Username: '',
+    usernameRules: [
+      v => !!v || 'Username is required',
+      v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+    ],
+    password: '',
+    passwordRules: [
+      v => !!v || 'Password is required',
+      v => (v && v.length >= 6) || 'Password must be 6  characters or more!',
+    ],
   }),
   methods:{
     submitHandler(){
-      console.log(this.Username)
-      this.loginSuccess = true;
+      if (this.$refs.form.validate()){
+          this.loading = true
+        setTimeout(()=> {
+          this.loading = false
+          this.snackbar = true
+        },3000)
+      }
     }
   }
-}
+};
 </script>
-
 <style>
-  .background{
+  .backgruond{
     background-image: url(./assets/Order-Banner.jpg) !important;
-    height: 370px;
+    height: 300px;
     width: 100%;
     display: block;
     position: absolute;
     top: 0;
     background-size: cover;
   }
-
 </style>
